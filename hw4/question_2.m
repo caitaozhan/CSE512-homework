4e29f69cd8e7f0d5d7fc58fb68c55a43b865f2c5
caitaozhan <caitaozhan@gmail.com>
2018-10-15 21:38:41 -0400

    solve question 2.1

--- hw4/question_2.m
+++ hw4/question_2.m
@@ -1,2 +1,19 @@
 data = load('q2_1_data.mat');
-[m,n]= size(data.trD);
+X     = data.trD;    % training
+y     = data.trLb;
+X_val = data.valD;   % validation
+y_val = data.valLb;
+
+[d, n] = size(X);    % dimension = d, number of rows = n
+
+f = -ones(n, 1);
+H = (X'*X).*(y*y');
+Aeq = y';            % equality constraint
+beq = 0;
+C = 0.1;
+lb = zeros(n, 1);    % bound constraint
+ub(1:n, 1:1) = C;
+A = [];
+b = [];
+
+alpha = quadprog(H,f,A,b,Aeq,beq,lb,ub);
